ca4a518531519c3622c95d333caf935238282235
Maxim <maximlisa@yandex.ru>
2025-12-11 19:12:12 +0300

    Обновление калькулятора
    Теперь калькулятор рассчитывает стоимость, основываясь на переданных ему
    параметрах

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
 from config import BASE_PRICE, COLORS, PARTS
 
 def calculate(part, color):
-    # Нужно доделать проверки и расчет
-    return 0  # Заглушка
+    if part not in PARTS:
+        return "Ошибка детали"
+    if color not in COLORS:
+        return "Ошибка цвета"
+    return BASE_PRICE * PARTS[part] * COLORS[color]
